b1a79556601be8661a17d7cdcafe6b1ca47f9798
apsteven <apsteven@ssloan.co.uk>
2020-04-19 10:50:08 +0100

    Create yx5300.h

---
+++ yx5300.h
@@ -0,0 +1,24 @@
+/*
+YX5300.h - Library for the YX5300 Serial MP3 Player (Catalex Module)
+
+Created by Steven Sloan 
+
+19 April 2020
+
+Released into the public domain
+*/
+
+#ifndef YX5300_h
+#define YX5300_h
+
+#include "Arduino.h"
+
+Class YX5300
+{
+  public:
+  
+  private:
+
+}
+
+#endif
